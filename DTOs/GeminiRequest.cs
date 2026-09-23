7f6b71d659a26254694d3e6d148e2ea252eafafe
eosocram <socramlmonteiro@gmail.com>
2026-09-23 19:22:19 -0300

    Adcionei DTOs de requisição e resposta do Gemini

---
+++ DTOs/GeminiRequest.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+namespace WhatsAppAiBot.DTOs;
+
+public class GeminiRequest
+{
+    [JsonPropertyName("contents")] 
+    public List<Content> Contents { get; set; } = new List<Content>(); 
+}
+
+public class Content
+{
+    [JsonPropertyName("parts")] 
+    public List<Part> Parts { get; set; } = new List<Part>();
+}
+
+public class Part
+{
+    [JsonPropertyName("text")] 
+    public string Text { get; set; } = string.Empty;
+}
---
+++ DTOs/GeminiResponse.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+namespace WhatsAppAiBot.DTOs;
+
+public class GeminiResponse
+{
+    [JsonPropertyName("candidates")] 
+    public List<Candidate> Candidates { get; set; } = new List<Candidate>();
+}
+
+public class Candidate
+{
+    [JsonPropertyName("content")] 
+    public Content Content { get; set; } = new Content();
+}
