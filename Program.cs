432e954dcfd21259113b2af686cc295c1085e83a
eosocram <socramlmonteiro@gmail.com>
2026-09-23 18:02:51 -0300

    validacao de config.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", (IConfiguration configuration) =>
+{
+    var modelo = configuration["Gemini:Model"];
+    bool haveKeyGemini = !string.IsNullOrEmpty(configuration["Gemini:ApiKey"]);
+    bool haveTokenWpp = !string.IsNullOrEmpty(configuration["WhatsApp:UserToken"]);
+    return new
+    {
+        modelo = modelo,
+        servico = "WhatsApp AI Bot",
+        haveKeyGemini = haveKeyGemini,
+        haveTokenWpp = haveTokenWpp
+        
+        
+    };
+});
 
 app.Run();
